116840e784ffc26290d528db622287083d2bb90b
emeryjk <133061564+emeryjk@users.noreply.github.com>
2023-05-10 00:45:15 +0000

    mogul S colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,4 +10,21 @@ const config = {
    
 };
 
-module.exports = config;
+module.exports = {
+  //...
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#0c64fa",
+          secondary: "#1be3f3",
+          accent: "#c4bcfb",
+          neutral: "#e4e4e4",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+}
