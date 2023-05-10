ca98312bd48d70eb0618fe3a61e748ce47f5d40c
emeryjk <133061564+emeryjk@users.noreply.github.com>
2023-05-10 00:21:35 +0000

    mogul adjustments 2

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,24 +8,6 @@ const config = {
   plugins: [require("daisyui")]
 
    
-  module.exports = {
-    //...
-    daisyui: {
-      themes: [
-        {
-          mytheme: {
-            primary: "#a991f7",
-            secondary: "#f6d860",
-            accent: "#37cdbe",
-            neutral: "#3d4451",
-            "base-100": "#ffffff",
-          },
-        },
-        "dark",
-        "cupcake",
-      ],
-    },
-  }
 };
 
 module.exports = config;
